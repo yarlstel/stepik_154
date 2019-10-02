af9cc8b0b5adfafc4793cd6ea67348fb2dec37b1
yarlstel <yarlstel@gmail.com>
2019-10-03 09:50:24 +1100

    Create server.py

---
+++ server.py
@@ -0,0 +1,11 @@
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)                           
+s.bind(('0.0.0.0', 2222))                                                       
+s.listen(5)                                                                     
+while True:
+  conn, addr = s.accept()                                                 
+  while True:                                                             
+    data = conn.recv(1024)                                          
+    if not data or data.decode() == 'close' : break
+    conn.send(data)
+  conn.close
+                
