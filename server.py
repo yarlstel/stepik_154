8eb65d7f403fa123301ae341358c328e0a41bb8c
yarlstel <yarlstel@gmail.com>
2019-10-03 09:54:00 +1100

    Update server.py

--- server.py
+++ server.py
@@ -1,3 +1,5 @@
+import socket
+
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)                           
 s.bind(('0.0.0.0', 2222))                                                       
 s.listen(5)                                                                     
