c436efe3d0eb95f1b8480d5873588adfa9a1294c
Yaroslav Stelmakh <yarlstel@gmail.com>
2019-10-06 23:44:18 +0100

    Parsing query

--- hello.py
+++ hello.py
@@ -1,5 +1,9 @@
 def app(environ, start_response):
-    data = b"Hello, World!\n"
+    data = b""
+    q = environ['QUERY_STRING'].split('&')
+    for i in q:
+        data = data + i.encode() + b"\n" #i.encode
+    print(data.decode())
     start_response("200 OK", [
         ("Content-Type", "text/plain"),
         ("Content-Length", str(len(data)))
