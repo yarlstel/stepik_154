6e54a5f9906bf11e1a2482a3637da0cdf73c3aa3
Yaroslav Stelmakh <yarlstel@gmail.com>
2019-10-07 05:31:03 +0100

    АААА! Слеши не в ту сторону!

--- upd.sh
+++ upd.sh
@@ -1,9 +1,9 @@
 sudo pip3 install django --upgrade
 sudo pip3 install gunicorn --upgrade
-sed -i -e 's/\/usr\/bin\/python/\/usr\/bin\/python3/' \usr\sbin\gunicorn-debian
-sed -i -e 's/\/usr\/bin\/python/\/usr\/bin\/python3/' \usr\bin\gunicorn
-sed -i -e 's/17.5/19.9.0/' \usr\bin\gunicorn
-sed -i -e 's/\/usr\/bin\/python/\/usr\/bin\/python3/' \usr\bin\gunicorn_django
-sed -i -e 's/17.5/19.9.0/' \usr\bin\gunicorn_django
-sed -i -e 's/\/usr\/bin\/python/\/usr\/bin\/python3/' \usr\bin\gunicorn_paster
-sed -i -e 's/17.5/19.9.0/' \usr\bin\gunicorn_paster
+sudo sed -i -e 's/\/usr\/bin\/python/\/usr\/bin\/python3/' /usr/sbin/gunicorn-debian
+sudo sed -i -e 's/\/usr\/bin\/python/\/usr\/bin\/python3/' /usr/bin/gunicorn
+sudo sed -i -e 's/17.5/19.9.0/' /usr/bin/gunicorn
+sudo sed -i -e 's/\/usr\/bin\/python/\/usr\/bin\/python3/' /usr/bin/gunicorn_django
+sudo sed -i -e 's/17.5/19.9.0/' /usr/bin/gunicorn_django
+sudo sed -i -e 's/\/usr\/bin\/python/\/usr\/bin\/python3/' /usr/bin/gunicorn_paster
+sudo sed -i -e 's/17.5/19.9.0/' /usr/bin/gunicorn_paster
