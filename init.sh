1f51b5825cbe4334a916540325ac4a0fb4764a45
Yaroslav Stelmakh <yarlstel@gmail.com>
2019-10-07 23:53:50 +0100

    Исправления в init.sh

--- init.sh
+++ init.sh
@@ -1,7 +1,9 @@
 sudo unlink /etc/nginx/sites-enabled/default
 sudo ln -s /home/box/web/etc/nginx.conf	/etc/nginx/sites-enabled/test.conf
 sudo /etc/init.d/nginx restart
-#sudo ln -s /home/box/web/etc/gunicorn.conf /etc/gunicorn.d/test
-gunicorn -c etc/gunicorn.conf hello:app
+sudo ln -s /home/box/web/etc/gunicorn.conf /etc/gunicorn.d/test
+sudo ln -s /home/box/web/etc/gunicorn.conf /etc/gunicorn.d/test
+sudo /etc/init.d/gunicorn restart
+#gunicorn -c etc/gunicorn.conf hello:app
 #sudo /etc/init.d/mysql start
 
