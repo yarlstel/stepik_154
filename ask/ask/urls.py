deebf6db0513340452d51ecaf73ceb44f63cd89a
Yaroslav Stelmakh <yarlstel@gmail.com>
2019-10-08 07:29:38 +0100

    Промежуточная настройка

--- ask/ask/urls.py
+++ ask/ask/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+form qa.views import test
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('question/<int:id>', test)
 ]
--- init.sh
+++ init.sh
@@ -1,8 +1,9 @@
 sudo unlink /etc/nginx/sites-enabled/default
+sudo unlink /etc/gunicorn.d/test
 sudo ln -s /home/box/web/etc/nginx.conf	/etc/nginx/sites-enabled/test.conf
 sudo /etc/init.d/nginx restart
 sudo ln -s /home/box/web/etc/gunicorn.conf /etc/gunicorn.d/test
-sudo ln -s /home/box/web/etc/gunicorn.conf /etc/gunicorn.d/test
+#sudo ln -s /home/box/web/etc/gunicorn.conf /etc/gunicorn.d/test
 sudo /etc/init.d/gunicorn restart
 #gunicorn -c etc/gunicorn.conf hello:app
 #sudo /etc/init.d/mysql start
