3ca7176f29616127d5649efd250ee02602579206
Yaroslav Stelmakh <yarlstel@gmail.com>
2019-10-09 00:11:10 +0100

    Проблемы с конфигурацией

--- ask/qa/views.py
+++ ask/qa/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
+from django.conf import  settings
 from django.http import HttpResponse
+
+settings.configure(DEBUG=True)
+
 # Create your views here.
 def test(request, *args, **kwargs):
     return HttpResponse('OK')
