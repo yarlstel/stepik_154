06aa0beb7fc3ccf8d8720f5e088d9796494e2a58
Yaroslav Stelmakh <yarlstel@gmail.com>
2019-10-09 04:36:44 +0100

    Устанавливаем Python 3.5 и делаем его основным

--- upd.sh
+++ upd.sh
@@ -1,3 +1,7 @@
+sudo apt update
+sudo apt install python3.5
+sudo update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.5 1
+
 sudo pip3 install django --upgrade
 sudo pip3 install gunicorn --upgrade
 sudo sed -i -e 's/\/usr\/bin\/python/\/usr\/bin\/python3/' /usr/sbin/gunicorn-debian
