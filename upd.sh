29b55a77aaabbbffb519e9459e45a1c2651a2918
Yaroslav Stelmakh <yarlstel@gmail.com>
2019-10-09 23:44:23 +0100

    Включаем SSL. Ещё нужна работа руками

--- upd.sh
+++ upd.sh
@@ -1,7 +1,7 @@
 sudo apt update
 sudo apt-get install build-essential checkinstall
 sudo apt-get install libreadline-gplv2-dev libncursesw5-dev libssl-dev \
-    libsqlite3-dev tk-dev libgdbm-dev libc6-dev libbz2-dev libffi-dev zlib1g-dev
+    libsqlite3-dev tk-dev libgdbm-dev libc6-dev libbz2-dev libffi-dev zlib1g-dev openssl
 cd ..
 mkdir tmp
 cd tmp
