d10015f2688ae028fb36bc0f46c152c20aaf0c15
Yaroslav Stelmakh <yarlstel@gmail.com>
2019-10-10 01:15:34 +0100

    свет в конце туннеля

--- ask/ask/settings.py
+++ ask/ask/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '+tx!nrh#rm^f(9yli=ge86=5wfe&7qg*sq4r7t(-(*6)jo8qhx'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['0.0.0.0']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
